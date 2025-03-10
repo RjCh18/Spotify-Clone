eba8c24a826ff1cbd017ba698d76fdb93e18245e
RjCh18 <rajatchaudharyjaat1809@gmail.com>
2025-03-11 02:49:16 +0530

    seekbar and audio backend

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 console.log("Running the script");
+let currentSong = new Audio()
+
+function formatTime(seconds) {
+    let minutes = Math.floor(seconds / 60) % 100; // Max two-digit minutes
+    let secs = Math.floor(seconds % 60); // Ensure no decimal places
+
+    return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+}
 
 async function getSongs() {
 
@@ -8,42 +16,92 @@ async function getSongs() {
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
-    let songs =[]
+    let songs = []
 
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3"))
+        if (element.href.endsWith(".mp3"))
             songs.push(element.href.split("/songs/")[1])
-        
+
     }
     return songs;
-    
-
 }
 
- async function main(){
+const playMusic = (track,pause=false) => {
+    // let audio = new Audio("/songs/"+ track)
+    currentSong.src = "/songs/" + track
+    if(!pause){
+        currentSong.play()
+        play.src = "pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = track.replaceAll('%20'," ").split("-")[0]
+    document.querySelector(".songtime").innerHTML = "00:00/00:00"
+}
 
-    let songs= await getSongs()
-    console.log(songs);
+async function main() {
+
+    let songs = await getSongs()
+    playMusic(songs[0],true)
 
     let songUl = document.querySelector(".songlist").getElementsByTagName("ul")[0]
-   for (const song of songs) {
-    const songTitle = song.replace('.mp3', '');
-        songUl.innerHTML += `<li>${songTitle.replaceAll("%20", " ")}</li>`;
-    
-   }
+    for (const song of songs) {
+        const songTitle = song.replace('.mp3', '');
+        songUl.innerHTML += `<li>
+                            <svg class="invert" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+                                <path d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z" stroke="currentColor" stroke-width="1.5" />
+                                <path d="M10 15.5C10 16.3284 9.32843 17 8.5 17C7.67157 17 7 16.3284 7 15.5C7 14.6716 7.67157 14 8.5 14C9.32843 14 10 14.6716 10 15.5ZM10 15.5V11C10 10.1062 10 9.65932 10.2262 9.38299C10.4524 9.10667 10.9638 9.00361 11.9865 8.7975C13.8531 8.42135 15.3586 7.59867 16 7V13.5M16 13.75C16 14.4404 15.4404 15 14.75 15C14.0596 15 13.5 14.4404 13.5 13.75C13.5 13.0596 14.0596 12.5 14.75 12.5C15.4404 12.5 16 13.0596 16 13.75Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                            <div class="info">
+                                <div>${songTitle.replaceAll("%20", " ").split("-")[0]}</div>
+                                <div>${songTitle.replaceAll("%20", " ").split("-")[1]}</div>
+                            </div>
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="invert"  viewBox="0 0 24 24" width="24" height="24"
+                            color="#000000" fill="none">
+                            <path
+                                d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                        </svg>
+                            </div>
+                    </li>`;
 
-    var audio = new Audio(songs[0]);
-    audio.play();
+    }
 
-    audio.addEventListener("loadeddata", () => {
-        console.log(audio.duration, audio.currentSrc,audio.currentTime);
+    document.querySelector(".songlist ul").addEventListener("click", (event) => {
+        let clickedLi = event.target.closest("li");
+        if (clickedLi) {
+            const songIndex = Array.from(clickedLi.parentElement.children).indexOf(clickedLi);
+            playMusic(songs[songIndex]);
+        }
+    });
+
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play()
+            play.src = "pause.svg"
+        }
+        else {
+            currentSong.pause()
+            play.src = "play.svg"
+        }
+    })
+
+
+    currentSong.addEventListener("timeupdate", ()=>{
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songtime").innerHTML = `${formatTime(currentSong.currentTime)} / ${formatTime(currentSong.duration)}`
+        document.querySelector(".circle").style.left=(currentSong.currentTime/currentSong.duration)*100 + "%"
         
-    }); 
- 
+    })
+
+    document.querySelector(".seekBar").addEventListener("click", e=>{
+        document.querySelector(".circle").style.left=(e.offsetX/e.target.getBoundingClientRect().width)*100 + "%";
+
+        currentSong.currentTime= (currentSong.duration*(e.offsetX/e.target.getBoundingClientRect().width)*100)/100
+    })
 }
 
-main() 
+main()
 
 
 
